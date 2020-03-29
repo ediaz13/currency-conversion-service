a1f642b087c00ea161aacb7ac5532e836c2a2f87
ediaz13 <diazemanuel27@gmail.com>
2020-03-29 20:42:32 -0300

    Primer commit Currency Conversion Service

---
+++ src/main/java/com/soma/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.soma.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.soma.microservices.currencyconversionservice")
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
